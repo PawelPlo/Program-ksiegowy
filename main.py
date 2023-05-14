850f96da84821d20b692a3a14332bc35208c33ad
Pawel Plociennik <p.plociennik@wp.pl>
2023-05-14 12:22:55 +0200

    Utworzono wstepna strukture programu

---
+++ main.py
@@ -0,0 +1,24 @@
+import pprint
+saldo=0
+konto=0
+konto=float(konto)
+linia_kredytowa=0
+print("     Program księgowy      \n")
+print("Aktualny stan konta wynosi:{} zl.\n".format(konto))
+print('''Wybierz opcje:
+Linia kredytowa - wpisz: 1
+Saldo (operacje na koncie) - wpisz: 2
+Stan magazynu (dane calosciowe) - wpisz: 3
+Znajdz produkt w magazynie - wpisz: 4
+Sprzedaz - wpisz: 5
+Zakup - wpisz: 6
+Historia zdarzen - wpisz: 7
+Wyjscie z programu - wpisz: "koniec"''')
+
+
+
+#pprint.pprint()
+
+#while True:
+    #wybor=input()
+    #if wybor
